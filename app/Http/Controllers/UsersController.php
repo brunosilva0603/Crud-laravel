f1f249e6d00b49c94ffddd0a1c3819de5e06497a
brunosilva0603 <bruunosrs@gmail.com>
2020-11-25 18:59:53 -0300

    Criando controller2

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,63 +2,43 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\UsuarioRequest;
-use App\Models\User;
-use App\Models\Usuario;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
 
 class UsersController extends Controller
-{/**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+{
     
     public function index()
     {
-        $users = Usuario::where('status',1)->get();
-        return view('usuarios.list', ['users' => $users]);
+        return view ('/usuarios');
     }
-
-    public function new()
+    
+    public function create()
     {
-        return view('usuarios.form');
+        //
     }
-
-    public function add(User $request)
+    
+    public function store(Request $request)
     {
-        $users = new Usuario;
-        $users = $users->create($request->all());
-        return Redirect::to('/usuarios');
+        //
     }
-
+    
+    public function show($id)
+    {
+        //
+    }
+    
     public function edit($id)
     {
-        $users = Usuario::findOrFail($id);
-        return view('usuarios.form', ['users' => $users]);
+        //
     }
-
-    public function update($id, User $request)
+    
+    public function update(Request $request, $id)
     {
-        $users = Usuario::findOrFail($id);
-        $users->update($request->all());
-        return Redirect::to('/usuarios');
+        //
     }
-
-    public function delete( $id, User $request ){
-        $users = Usuario::where($id);
-        $users->update($request->status(0));
-        return Redirect::to('/usuarios');
-
-
-        //$users = $this->input->post('status');
-        //$queryVar = ["status" => 1];
-        //$this->db->where('id', $dados->idUsuario);
-        //$this->db->update('minhaTabelaDeUsuarios', $queryVar);
+    
+    public function destroy($id)
+    {
+        //
     }
 }
